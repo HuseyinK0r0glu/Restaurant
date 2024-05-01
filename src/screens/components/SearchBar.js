f3c9c96e56919469a4ca8c5fbafca5355a578903
HuseyinK0r0glu <korogluh27@gmail.com>
2024-05-01 21:15:37 +0300

    added state for text input in screen bar

--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -2,12 +2,15 @@ import React from "react";
 import {View,TextInput,StyleSheet} from "react-native"
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ( {term , onTermChange}) => {
+
     return <View style = {styles.backgroundStyle}>
         <Feather name = "search" style = {styles.iconStyle}/>
         <TextInput
             style = {styles.inputStyle}
             placeholder="Search"
+            value={term}
+            onChangeText={ newTerm => onTermChange(newTerm)}
         />
     </View>
 }
