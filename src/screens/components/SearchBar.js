5d8991d43ca4b2c4c697fe06e5f07d33a4473817
HuseyinK0r0glu <korogluh27@gmail.com>
2024-05-01 20:53:37 +0300

    Restaurant app project uploaded to GitHub

---
+++ src/screens/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {View,TextInput,StyleSheet} from "react-native"
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = () => {
+    return <View style = {styles.backgroundStyle}>
+        <Feather name = "search" style = {styles.iconStyle}/>
+        <TextInput
+            style = {styles.inputStyle}
+            placeholder="Search"
+        />
+    </View>
+}
+
+const styles = StyleSheet.create({
+    backgroundStyle:{
+        marginTop : 10,
+        /// hex code for color ==>  #F0EEEE == rgb(240,240,240)
+        backgroundColor: '#F0EEEE',
+        height:50,
+        borderRadius:5,
+        marginHorizontal:15,
+        flexDirection: 'row'
+    }, 
+    inputStyle : {
+        flex : 1,
+        fontSize : 18
+    },
+    iconStyle:{
+        fontSize : 35,
+        alignSelf : 'center',
+        marginHorizontal : 15
+    }
+
+});
+
+export default SearchBar;
