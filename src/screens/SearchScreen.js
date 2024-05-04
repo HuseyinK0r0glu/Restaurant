db85f318252c8206bb9af35474bbc3e7d2548a6e
HuseyinK0r0glu <korogluh27@gmail.com>
2024-05-04 22:51:00 +0300

    extract hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,31 +2,11 @@ import React , {useState , useEffect} from 'react';
 import { View,Text,StyleSheet } from 'react-native';
 import SearchBar from './components/SearchBar';
 import yelp from './api/yelp';
+import useResults from './hooks/useResults';
 
 const SearchScreen = () => {
-    const [term , setTerm] = useState('');  
-    const [results,setResults] = useState([]);
-    const [errorMessage,setErrorMessage] = useState('');
-
-    const searchApi = async (searchTerm) => {
-        try{
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose' 
-                 }
-            });
-            setResults(response.data.businesses);
-        }catch (e){
-            console.log(e)
-            setErrorMessage('Something went wrong');
-        }
-    };
-
-   useEffect(() => {    
-        searchApi('pasta')
-    }, [])
+    const [term , setTerm] = useState('');
+    const [searchApi,results,errorMessage] = useResults();
 
     return <View>
         <SearchBar 
