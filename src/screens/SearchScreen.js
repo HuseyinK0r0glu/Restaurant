061ca999c8a17966c2505e07faad18905a6b319e
HuseyinK0r0glu <korogluh27@gmail.com>
2024-05-06 07:34:42 +0300

    get list of restaurants and show them

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,20 @@
-import React , {useState , useEffect} from 'react';
+import React , {useState} from 'react';
 import { View,Text,StyleSheet } from 'react-native';
 import SearchBar from './components/SearchBar';
 import yelp from './api/yelp';
 import useResults from './hooks/useResults';
+import ResultsList from './components/ResultsList';
 
 const SearchScreen = () => {
     const [term , setTerm] = useState('');
     const [searchApi,results,errorMessage] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        // price === '$' || price === '$$' || price === '$$$'
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
 
     return <View>
         <SearchBar 
@@ -16,6 +24,9 @@ const SearchScreen = () => {
         />
         {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {results.length} results</Text>
+        <ResultsList results = {filterResultsByPrice('$')} title = "Cost Effective"/>
+        <ResultsList results = {filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultsList results = {filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
 }
 
