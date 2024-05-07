f89843833ac5ae7391d41cf9db094be61f86fc06
HuseyinK0r0glu <korogluh27@gmail.com>
2024-05-07 07:33:29 +0300

    images added and styled

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React , {useState} from 'react';
-import { View,Text,StyleSheet } from 'react-native';
+import { View,Text,StyleSheet,ScrollView} from 'react-native';
 import SearchBar from './components/SearchBar';
 import yelp from './api/yelp';
 import useResults from './hooks/useResults';
@@ -9,6 +9,8 @@ const SearchScreen = () => {
     const [term , setTerm] = useState('');
     const [searchApi,results,errorMessage] = useResults();
 
+    console.log(results);
+
     const filterResultsByPrice = (price) => {
         // price === '$' || price === '$$' || price === '$$$'
         return results.filter(result => {
@@ -16,7 +18,7 @@ const SearchScreen = () => {
         });
     };
 
-    return <View>
+    return <View style = {{flex : 1}}>
         <SearchBar 
             term = {term} 
             onTermChange = {setTerm} 
@@ -24,10 +26,13 @@ const SearchScreen = () => {
         />
         {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {results.length} results</Text>
+        <ScrollView>
         <ResultsList results = {filterResultsByPrice('$')} title = "Cost Effective"/>
         <ResultsList results = {filterResultsByPrice('$$')} title="Bit Pricier" />
         <ResultsList results = {filterResultsByPrice('$$$')} title="Big Spender" />
+        </ScrollView>
     </View>
+
 }
 
 const styles = StyleSheet.create({
--- src/screens/api/yelp.js
+++ src/screens/api/yelp.js
@@ -7,3 +7,5 @@ export default axios.create({
         'Bearer T97YMWlFK4HJfVk5WZSQIJC3t3U3E-2FVAir-rsiHod3QAwhNww1sM4sx5hIuIycDIXTJtqdssEq0YqW9aM_RlV9dk3lF0ee2d47FOJjXowQDJ4dIkDdRimq9AozZnYx',
     }
 });
+
+
