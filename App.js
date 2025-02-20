c14b0b84de436334eecfd9f399a5ee01657d743f
HuseyinK0r0glu <korogluh27@gmail.com>
2025-02-20 16:43:20 +0300

    updated to react navigation v6

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-
-import SearchScreen from './src/screens/SearchScreen';
-import ResultsShowScreen from "./src/screens/ResultShowScreen";
-
-const navigator = createStackNavigator(
-  {
-    Search : SearchScreen,
-    ResultsShow : ResultsShowScreen
-  },
-  {
-  initialRouteName: "Search",
-  defaultNavigationOptions: {
-    title: "Business Search",
-    },
-  }
-);
-
-export default createAppContainer(navigator);
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+ 
+import SearchScreen from "./src/screens/SearchScreen";
+import ResultsShowScreen from "./src/screens/ResultsShowScreen";
+ 
+const Stack = createStackNavigator();
+ 
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerTitle: "Business Search" }}>
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="ResultsShow" component={ResultsShowScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
